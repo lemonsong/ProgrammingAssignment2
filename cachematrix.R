b2b0e45b9b212b1560e8ad70d029a7a554279a71
lemonsong <yilin@kis-MacBook-Air.local>
2014-06-19 18:19:06 +0800

    programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## reverse matrix
 
-## Write a short comment describing this function
+## reverse matrix x and return the inverse of "x"
 
 makeCacheMatrix <- function(x = matrix()) {
+  +     row<-nrow(x)
+  +     col<-ncol(x)
+  +     y=matrix(1:row*col,nrow=col,ncol=row)
+  +     for (i in 1:row) for (j in 1:col) y[j,i]<-x[i,j]
+  +     y
+  + }
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
